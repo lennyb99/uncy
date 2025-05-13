11fb0ef8ffcf2eae9e0b44cbcf75e95d05455671
lennyb99 <lennogle@gmail.com>
2025-05-13 15:38:03 +0200

    setup

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Gib deinen Namen ein:");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Hallo, {name}!");
+    }
+}
