6c0f603b67717b51b94e7fbad4f87d2d8fef7263
lennyb99 <lennogle@gmail.com>
2026-01-23 14:13:51 +0100

    added board building

--- Uncy.Engine/Program.cs
+++ Uncy.Engine/Program.cs
@@ -18,6 +18,8 @@ class Program
         Fen fenBishop = new Fen("8/8/8/8/3B4/8/8/8 w KQkq - 0 1");
         Fen fenKnight = new Fen("k7/8/8/8/3N4/8/8/K7 w KQkq - 0 1");
 
+        Fen bigFen = new Fen("10/10/pppp4pp/10/10/10 w KQkq - 0 1");
+
         Fen tempFen = new Fen("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10");
 
         Fen debugFen = new Fen("r2q1rk1/1bp2p2/pb1p1n2/1p2n1B1/3p4/1BPQ4/P4PPP/RN2R1K1 w - - 0 1");
@@ -26,14 +28,14 @@ class Program
         Fen closedPositionWithBishops = new Fen("5b2/3k4/1p1p1p1p/pPpPpPpP/P1P1P1P1/8/3BK3/8 w - - 0 1");
         Fen mateToFind = new Fen("r2r2k1/pp3ppp/2p2b2/5q2/4RB2/1P3PP1/P4P1P/3QR1K1 w - - 0 1");
 
-        Board board = new Board(debugFen);
+        Board board = new Board(bigFen);
         //board.PrintBoardToConsole();
 
         //Console.WriteLine(board.ToFen());
 
         // Original Perft (misst viel mehr als nur Move-Generation)
         //StartPerftDebug(board, 5);
-        StartSearch(board, 5);
+        //StartSearch(board, 5);
 
         // Vergleich: Alle Perft-Varianten
         //CompareAllPerftVariants(board, 5);
